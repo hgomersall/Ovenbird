dc057abe6fe8b00c080047a93b5ffe1ea295f34c
Henry Gomersall <heng@smartacoustics.co.uk>
2016-09-30 16:00:14 +0100

    Added a Vivado version attribute to the module.

--- ovenbird/__init__.py
+++ ovenbird/__init__.py
@@ -1,5 +1,18 @@
 from distutils import spawn as _spawn
+import subprocess as _subprocess
+
 VIVADO_EXECUTABLE = _spawn.find_executable('vivado')
+
+if VIVADO_EXECUTABLE is not None:
+    vivado_version_exe = _subprocess.Popen(
+        [VIVADO_EXECUTABLE, '-version'], stdin=_subprocess.PIPE,
+        stdout=_subprocess.PIPE, stderr=_subprocess.PIPE)
+
+    out, err = vivado_version_exe.communicate()
+    VIVADO_VERSION = out.split()[1][1:]
+
+else:
+    VIVADO_VERSION = None
 
 from .cosimulation import *
 from .vivado_ip import *
