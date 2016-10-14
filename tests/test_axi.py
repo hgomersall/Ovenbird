81cec6a5e292f04f8bb48e4fef26bbe4fb5b2193
Henry Gomersall <heng@smartacoustics.co.uk>
2016-10-14 17:04:56 +0100

    Added the axi tests to cover the convertible case from upstream veriutils.

---
+++ tests/test_axi.py
@@ -0,0 +1,18 @@
+from veriutils.tests.test_axi import TestAxiMasterPlaybackBlockMinimal
+from ovenbird import vivado_verilog_cosimulation, vivado_vhdl_cosimulation
+
+class TestAxiMasterPlaybackBlockMinimalVivadoVHDL(
+    TestAxiMasterPlaybackBlockMinimal):
+    def sim_wrapper(self, sim_cycles, dut_factory, ref_factory,
+                           args, arg_types, **kwargs):
+
+        return vivado_vhdl_cosimulation(
+            sim_cycles, dut_factory, ref_factory, args, arg_types, **kwargs)
+
+class TestAxiMasterPlaybackBlockMinimalVivadoVerilog(
+    TestAxiMasterPlaybackBlockMinimal):
+    def sim_wrapper(self, sim_cycles, dut_factory, ref_factory,
+                           args, arg_types, **kwargs):
+
+        return vivado_verilog_cosimulation(
+            sim_cycles, dut_factory, ref_factory, args, arg_types, **kwargs)
